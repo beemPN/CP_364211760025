b5de158fd2bab0aaf2bd31f7a9fd509e5d584684
beem <pannika.n@rmutsvmail.com>
2022-04-08 02:42:07 +0700

    test uploed

---
+++ LAB7/ex_if.py
@@ -0,0 +1,14 @@
+<=
+!=
+and * --> 1*0*1*1*1 = 0, 1 1 1 1 1 = 1
+or  + --> 1+1+0+0+0 = 1, 0 0 0 0 0 = 0
+"""
+# if
+print('Start')
+
+x = 100
+if x<100:
+    print('x is less than 100')
+elif x>100:
+    print('x is more than 100')
+e
---
+++ LAB7/ex_ifelse.py
@@ -0,0 +1,13 @@
+#example program: Login
+
+username='admin'
+password='1234'
+
+u = input('Enter username: ')
+p = input('Enter password: ')
+
+
+# if u == username and p == password:
+#    print(f'Welcome {u}')
+# else:
+#    print('our username or password is notcorrect.')
